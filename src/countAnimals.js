f08c3a623cbd72aa64a56e1f3e4ff134f2c38c37
Guidzs <guidz.2004@gmail.com>
2022-06-29 19:39:08 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const result = {};
+    species.forEach((specie) => {
+      const { name, residents } = specie;
+      result[name] = residents.length;
+    });
+    return result;
+  }
+  const { specie, sex } = animal;
+  const { residents } = species.find((elmt) => elmt.name === specie);
+  if (sex === undefined) { return residents.length; }
+  return residents.filter((resident) => resident.sex === sex).length;
 }
 
 module.exports = countAnimals;
