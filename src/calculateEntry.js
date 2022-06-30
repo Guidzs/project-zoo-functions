fff5bfeddf2a06a04aed738cbc465ad2774ad61f
Guidzs <guidz.2004@gmail.com>
2022-06-30 00:04:17 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = {
+    adult: 0,
+    child: 0,
+    senior: 0,
+  };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) { result.child += 1; }
+    if (entrant.age >= 18 && entrant.age < 50) { result.adult += 1; }
+    if (entrant.age >= 50) { result.senior += 1; }
+  });
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) { return 0; }
+  const { adult, child, senior } = countEntrants(entrants);
+  const { prices } = data;
+
+  return adult * prices.adult + child * prices.child + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
