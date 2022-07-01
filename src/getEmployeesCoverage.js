554f4e1533449e7e86a195ba2f2523f47bb249ca
Guidzs <guidz.2004@gmail.com>
2022-06-30 21:46:22 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const createObj = (employee) => {
+  const { id, firstName, lastName, responsibleFor } = employee;
+  const animals = species
+    .filter((specie) => responsibleFor.includes(specie.id))
+    .map((specie) => specie.name);
+  const locations = species
+    .filter((specie) => responsibleFor.includes(specie.id))
+    .map((specie) => specie.location);
+  return {
+    id,
+    fullName: `${firstName} ${lastName}`,
+    species: animals,
+    locations,
+  };
+};
+
+const createObjCmplt = () => employees.map((employee) => createObj(employee));
+
+function getEmployeesCoverage(nI) {
+  if (nI === undefined) return createObjCmplt();
+  const verifyNOrI = employees.find((employee) => {
+    if (employee.id === nI.id || employee.firstName === nI.name || employee.lastName === nI.name) {
+      return employee;
+    }
+    return undefined;
+  });
+  if (verifyNOrI === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return createObj(verifyNOrI);
 }
+
+// console.log(getEmployeesCoverage(''));
 
 module.exports = getEmployeesCoverage;
