7156cfcc2c6c872ef4898bb945e860ceb8c3d38c
Guidzs <guidz.2004@gmail.com>
2022-07-01 14:39:33 -0300

    requisitos 6, 7 e 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,41 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('testa se a fução ao não receber parametros retorna undefined', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+
+  it('testa se ao passar um parametro invalido retorna o erro', () => {
+    const actual = handlerElephants(Number);
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+
+  it('testa se caso o parametro seja igual ao uma chave retorna o valor', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('testa se ao passar o parametro certo retona o esperado', () => {
+    {
+      const actual = handlerElephants('count');
+      const espected = 4;
+      expect(actual).toBe(espected);
+    }
+    {
+      const actual = handlerElephants('names');
+      const espected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+      expect(actual).toEqual(espected);
+    }
+    {
+      const actual = handlerElephants('averageAge');
+      const espected = 10.5;
+      expect(actual).toBe(espected);
+    }
+    {
+      const actual = handlerElephants('qualque coisa');
+      const espected = null;
+      expect(actual).toBe(espected);
+    }
+  });
+});
